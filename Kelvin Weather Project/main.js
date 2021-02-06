f24e5b2d9fec21334e9850249bdd46086b6f6a2a
Paridhi Jain <18ec.paridhijain@gmail.com>
2021-02-06 12:12:47 +0530

    Add files via upload

---
+++ Kelvin Weather Project/main.js
@@ -0,0 +1,11 @@
+let kelvinTemp = 301;
+// Value of temperature in kelvin
+
+let celsiusTemp = kelvinTemp - 273.15;
+// Value we need in celsius
+
+let FahrenheitTemp = celsiusTemp * (9/5) + 32;
+// value of temperature in fahrenhiet
+
+console.log("The temperature in degrees Fahrenheit:" + Math.floor(FahrenheitTemp));
+
