b4801b86e1e0ec6150faa3741b1f7780ae921d02
Paridhi Jain <18ec.paridhijain@gmail.com>
2021-02-07 20:21:08 +0530

    Add files via upload

---
+++ Dog Years Project/main.js
@@ -0,0 +1,8 @@
+function calculateDogAge(age){
+    let dogYears = 7 * age;
+    console.log("Your doggie is" + dogyears + " years old in dog years!");
+}
+
+calculateDogAge(1);
+calculateDogAge(12);
+calculateDogAge(0.5);
